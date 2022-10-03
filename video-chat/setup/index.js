9759397c307c8a5bf3a7fbf1dc64d16efa81ed1c
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-10-03 02:27:04 +0200

    created server

--- video-chat/setup/index.js
+++ video-chat/setup/index.js
@@ -1,13 +1,35 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
+const io = require('socket.io')(server,{
+  cors:{
+    origin:'*',
+    methods:['GET','POST']
+  }
+})
 
 
 const port = 5000 || process.env.PORT 
 
-
 app.use(cors());
+app.get('/',(req,res)=>{
+  res.send('<h1>My server is running </h1>')
+})
 
+io.on('connection',(socket)=>{
+  socket.emit('me',socket.id)
 
+  socket.on('disconnect',()=> {
+    socket.broadcast.emit('callended')
+  })
+
+  socket.on('calluser',({userToCall,signalData,from,name}) => {
+    io.to(userToCall).emit('calluser',{signal:signalData,from:name})
+  } )
+
+  socket.on('answerCall',(data)=>{
+    io.to(data.to).emit('callaccepted',data.signal)
+  })
+})
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
