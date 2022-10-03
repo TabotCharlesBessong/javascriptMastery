282a02a2149d5b482f12160bec3752476b046cb3
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-10-03 02:39:36 +0200

    adding styles to App.js

--- video-chat/setup/client/src/App.js
+++ video-chat/setup/client/src/App.js
@@ -1,13 +1,40 @@
 import React from 'react'
-import { Typography , AppBar } from '@material-ui/core'
+import { Typography , AppBar, makeStyles } from '@material-ui/core'
 import { Notifications, Sidebar, VideoPlayer } from './components'
 
+const useStyles = makeStyles((theme) => ({
+	appBar: {
+		borderRadius: 15,
+		margin: "30px 100px",
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		width: "600px",
+		border: "2px solid black",
+
+		[theme.breakpoints.down("xs")]: {
+			width: "90%",
+		},
+	},
+	image: {
+		marginLeft: "15px",
+	},
+	wrapper: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		width: "100%",
+	},
+}));
+
 const App = () => {
+  const classes = useStyles()
   return (
-		<div>
-			<AppBar position="static" color="inherit">
+		<div className={classes.wrapper}>
+			<AppBar className={classes.appBar} position="static" color="inherit">
 				<Typography variant="h2" align="center">
-					video chat
+					Video Chat
 				</Typography>
 			</AppBar>
 			<VideoPlayer />
