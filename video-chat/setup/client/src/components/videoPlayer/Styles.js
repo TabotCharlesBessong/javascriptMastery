b722707a4332bb94ab4c4cb6079ca95a57ed33a7
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-10-03 02:10:59 +0200

    added the mui styles

--- video-chat/setup/client/src/components/videoPlayer/Styles.js
+++ video-chat/setup/client/src/components/videoPlayer/Styles.js
@@ -1,5 +1,21 @@
 import { makeStyles } from "@material-ui/core";
 
-export default makeStyles((theme)=>({
-  
-}))
+export default makeStyles((theme) => ({
+	video: {
+		width: "550px",
+		[theme.breakpoints.down("xs")]: {
+			width: "300px",
+		},
+	},
+	gridContainer: {
+		justifyContent: "center",
+		[theme.breakpoints.down("xs")]: {
+			flexDirection: "column",
+		},
+	},
+	paper: {
+		padding: "10px",
+		border: "2px solid black",
+		margin: "10px",
+	},
+}));
