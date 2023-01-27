f2f1d7a71d18f5d674e51b377276b99314c1c371
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-27 15:27:18 +0100

    done creating the schemas

--- PortfolioSite/sanity_backend/schemas/index.js
+++ PortfolioSite/sanity_backend/schemas/index.js
@@ -1,1 +1,11 @@
-export const schemaTypes = []
+
+import testimonials from './testimonials'
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import experience from './experience'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [testimonials,about,brands,contact,experience,skills,workExperience,works]
