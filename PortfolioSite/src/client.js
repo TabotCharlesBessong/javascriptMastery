98d25e52f138b35796425a95a802cd91c5e3e672
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-28 09:25:19 +0100

    taking a break with a lot of issues

---
+++ PortfolioSite/src/client.js
@@ -0,0 +1,16 @@
+
+import sanityClient  from "@sanity/client";
+// import { SanityClient } from "@sanity/client";
+import ImageUrlBuilder from "@sanity/image-url";
+
+export const client = sanityClient({
+	projectId: "mx9tc1uq",
+	dataset: "production",
+	apiVersion: "2023-01-28",
+	useCdn: true,
+	token: import.meta.env.REACT_APP_SANITY_TOKEN,
+});
+
+const builder = ImageUrlBuilder(client)
+
+export const urlFor = (source) => builder.image(source)
--- PortfolioSite/src/container/about/About.jsx
+++ PortfolioSite/src/container/about/About.jsx
@@ -1,11 +1,69 @@
 
+import React,{useState,useEffect} from 'react'
+import './About.scss'
+import {motion} from 'framer-motion'
+import images from '../../constants/images'
+import { urlFor,client } from '../../client'
 
-import React from 'react'
+// const abouts = [
+// 	{
+// 		title: "Web Development",
+// 		description: "I am a good developer",
+// 		imgUrl: images.about01,
+// 	},
+// 	{
+// 		title: "Web Design",
+// 		description: "I am a good designer",
+// 		imgUrl: images.about02,
+// 	},
+// 	{
+// 		title: "App Development",
+// 		description: "I am a good native developer",
+// 		imgUrl: images.about03,
+// 	},
+// 	{
+// 		title: "Project Management",
+// 		description: "I am a good project manager",
+// 		imgUrl: images.about04,
+// 	},
+// ];
 
 const About = () => {
+
+	const [abouts, setAbouts] = useState([])
+
+	useEffect(()=>{
+		const query = '*[_type =="abouts"]'
+		client.fetch(query).then((data) => setAbouts(data))
+	},[])
+	console.log(abouts)
   return (
-    <div>Hello About US</div>
-  )
+		<>
+			<h2 className="head-text">
+				I Know that <span>Good Development </span> <br /> means <span>Good Business</span>
+			</h2>
+
+			<div className="app__profiles">
+				{abouts.map((about, index) => (
+					<motion.div
+						whileInView={{ opacity: 1 }}
+						whileHover={{ scale: 1.1 }}
+						transition={{ duration: 0.5, type: "tween" }}
+						className="app__profile-item"
+						key={about.title + index}
+					>
+						<img src={about.imgUrl} alt="" />
+						<h2 className="bold-text" style={{ marginTop: 20 }}>
+							{about.title}
+						</h2>
+						<p className="p-text" style={{ marginTop: 10 }}>
+							{about.description}
+						</p>
+					</motion.div>
+				))}
+			</div>
+		</>
+	);
 }
 
 export default About
